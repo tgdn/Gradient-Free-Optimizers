9757ca83fe0ae1cde8bbe17f595b784c5b65fc28
Thomas Gak Deluen <tgdn45@gmail.com>
2021-11-09 12:50:22 +0100

    Add initial class for GridSearch

---
+++ gradient_free_optimizers/optimizers/grid_search.py
@@ -0,0 +1,20 @@
+# Author: ...
+# Email: ..
+# License: MIT License
+
+
+from .base_optimizer import BaseOptimizer
+from ..search import Search
+
+
+class GridSearch(BaseOptimizer, Search):
+    def __init__(
+        self,
+        search_space,
+        initialize={"grid": 4, "random": 2, "vertices": 4},
+    ):
+        super().__init__(search_space, initialize)
+
+    @BaseOptimizer.track_nth_iter
+    def iterate(self):
+        return self.move_random()
